5951afe0428223a406c00790f70ed36d064edcdc
Tyler <tyler.le@edu.sait.ca>
2023-05-08 16:52:12 -0600

    Fixes main branch

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">test</div>
-  );
+  return <div className="App">test</div>;
 }
 
 export default App;
