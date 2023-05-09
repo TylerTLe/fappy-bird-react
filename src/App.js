21b7170418da27a290c58a6b4a9007e73b1df069
Tyler <tyler.le@edu.sait.ca>
2023-05-08 22:11:55 -0600

    Adds Bird

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+const BIRD_SIZE = 20;
 function App() {
-  return <div className="App">test</div>;
+  return <Div>
+    <Bird size={BIRD_SIZE} />
+  </Div>;
 }
 
 export default App;
+
+const Bird = styled.div`
+  posistion: absolute;
+  background-color: red;
+  height: ${(props) => props.size}px;
+  width: ${(props) => props.size}px;
+  top: ${(props) => props.size}px;
+  border-radius: 50%
+`;
+
+const Div = styled.div`
+  display: flex;
+  width 100;
+  justify-content: center;
+`; 
+
+const GameBox = styled.div`
+ height: ${(props) => props.height}px;
+ width: ${(props) => props.height}px;
+ background-color: blue;
+`;
